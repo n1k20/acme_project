bab35e3682bb34da5b2aeb17c0556fa9a0d7f9ba
Nikolay Borgoyakov <nik.borgoyakov@bk.ru>
2026-07-05 21:09:29 +0700

    do many others thinks

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField(verbose_name='Имя', max_length=20)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=20, blank=True, help_text='Необязательное поле')
+    birthday = models.DateField(verbose_name='Дата рождения')
